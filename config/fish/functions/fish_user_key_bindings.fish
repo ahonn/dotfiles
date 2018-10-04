8e093dbf22ec03f5ccf0073d1e2984d26a75d88c
ahonn <ahonn95@outlook.com>
2018-10-04 15:54:10 +0800

    feat: add fish visual mode bindings

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
   # vi mode esc mapping
   bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+  bind -M visual jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 
   # clear line
   bind -M insert \cc kill-whole-line force-repaint
@@ -10,7 +11,9 @@ function fish_user_key_bindings
 
   # faster beginning & end of line
   bind -M default H beginning-of-line
+  bind -M visual H beginning-of-line
   bind -M default L end-of-line
+  bind -M visual L end-of-line
 
   # bind thefuck
   bind -M default \cf "fuck"
