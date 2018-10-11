36a781c0aeea139a792b30fa3d94f2dc4ae722b6
ahonn <ahonn95@outlook.com>
2018-10-11 10:06:53 +0800

    feat: disable jk map to esc, again(include fish user key bindings)

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,7 @@
 function fish_user_key_bindings
   # vi mode esc mapping
-  bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
-  bind -M visual jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+  # bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+  # bind -M visual jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 
   # clear line
   bind -M insert \cc kill-whole-line force-repaint
