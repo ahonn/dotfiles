5134cbd5f70b2bb17b870e51a8bc0e9d41053ac1
ahonn <ahonn95@outlook.com>
2018-12-25 22:57:47 +0800

    fix(fish): fixed path

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -1,13 +1,4 @@
 # ========== MIRROR ========== #
-set -gx IOJS_ORG_MIRROR https://npm.taobao.org/mirrors/iojs
-set -gx NODIST_IOJS_MIRROR https://npm.taobao.org/mirrors/iojs
-set -gx NVM_IOJS_ORG_MIRROR https://npm.taobao.org/mirrors/iojs
-set -gx NVMW_IOJS_ORG_MIRROR https://npm.taobao.org/mirrors/iojs
-set -gx NODEJS_ORG_MIRROR https://npm.taobao.org/mirrors/node
-set -gx NODIST_NODE_MIRROR https://npm.taobao.org/mirrors/node
-set -gx NVM_NODEJS_ORG_MIRROR https://npm.taobao.org/mirrors/node
-set -gx NVMW_NODEJS_ORG_MIRROR https://npm.taobao.org/mirrors/node
-set -gx NVMW_NPM_MIRROR https://npm.taobao.org/mirrors/npm
 set -gx ELECTRON_MIRROR https://npm.taobao.org/mirrors/electron/
 
 # =========== PATH =========== #
