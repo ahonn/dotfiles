18112c533bb5ff53f1d47ee410e3cdf656baaa0e
ahonn <ahonn95@outlook.com>
2019-04-03 16:33:24 +0800

    feat(fish): update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,10 @@
 set -gx DOTFILES $HOME/.dotfiles
 
 set -gx LANG en_US.UTF-8
+set -gx TERM 'screen-256color'
+
 set -gx EDITOR 'nvim'
 set -gx GIT_EDITOR 'nvim'
-
-source $DOTFILES/config/fish/aliases.fish
-source $DOTFILES/config/fish/path.fish
 
 # vi mode
 set -U fish_key_bindings fish_vi_key_bindings
@@ -14,4 +13,7 @@ set -U fish_cursor_insert line
 # disable greeting
 function fish_greeting
 end
+
+source $DOTFILES/config/fish/aliases.fish
+source $DOTFILES/config/fish/path.fish
 
