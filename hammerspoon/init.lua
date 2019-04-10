0b0db4fb613173fb08b003b74d8f1b4ff2e380b8
ahonn <ahonn95@outlook.com>
2019-04-10 22:21:53 +0800

    feat(hammerspoon): add auto load config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,2 +1,22 @@
 require "modules.window"
 require "modules.application"
+
+function reloadConfigCallback(files)
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+    hs.notify.new({
+      title="Hammerspoon",
+      informativeText="Reload Config Success"
+    }):send()
+  end
+end
+
+local configPath = os.getenv("HOME") .. "/.hammerspoon/"
+reloadWatcher = hs.pathwatcher.new(configPath, reloadConfigCallback):start()
+
