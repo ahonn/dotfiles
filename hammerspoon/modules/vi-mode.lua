f64c32b7db71ee14a8e5060d3db64e5990f86fa5
ahonn <ahonn95@outlook.com>
2019-04-19 16:51:17 +0800

    feat(hammerspoon): add more vi-mode support

--- hammerspoon/modules/vi-mode.lua
+++ hammerspoon/modules/vi-mode.lua
@@ -5,29 +5,39 @@ local eventtap = require "hs.eventtap"
 function tapperCallback(evt)
   local flags = evt:getFlags()
   local keyCode = evt:getKeyCode()
+  local char = evt:getCharacters()
 
-  function modsEquals(keys)
-    for i, key in ipairs(keys) do
-      if not flags[key] then
-        return false
-      end
+  local holdAlt = not flags['ctrl'] and not flags['shift'] and flags['alt']
+  if holdAlt then
+    if char == "h" then
+      -- bind alt + h as left
+      return true, {hs.eventtap.event.newKeyEvent({}, "left", true)}
+    elseif char == "j" then
+      -- bind alt + j as down
+      return true, {hs.eventtap.event.newKeyEvent({}, "down", true)}
+    elseif char == "k" then
+       -- bind alt + k as up
+      return true, {hs.eventtap.event.newKeyEvent({}, "up", true)}
+    elseif char == 'l' then
+       -- bind alt + l as right
+      return true, {hs.eventtap.event.newKeyEvent({}, "right", true)}
+    elseif char == '∫' then
+       -- bind alt + b as before world
+      return true, {hs.eventtap.event.newKeyEvent({ "alt" }, "left", true)}
+    elseif char == '∑' then
+       -- bind alt + w as after world
+      return true, {hs.eventtap.event.newKeyEvent({ "alt" }, "right", true)}
     end
-    return true
   end
 
-  if modsEquals({ "alt" }) then
-    if keyCode == 4 then
-      -- ctrl + h as left
-      return true, {hs.eventtap.event.newKeyEvent({}, "left", true)}
-    elseif keyCode == 38 then
-      -- ctrl + j as bottom
-      return true, {hs.eventtap.event.newKeyEvent({}, "down", true)}
-    elseif keyCode == 40 then
-      -- ctrl + k as up
-      return true, {hs.eventtap.event.newKeyEvent({}, "up", true)}
-    elseif keyCode == 37 then
-      -- ctrl + l as right
-      return true, {hs.eventtap.event.newKeyEvent({}, "right", true)}
+  local holdAltAndShift = not flags['ctrl'] and flags['shift'] and flags['alt']
+  if holdAltAndShift then
+    if char == "Ó" then
+      -- bind alt + shift + h as start of line
+      return true, {hs.eventtap.event.newKeyEvent({ "ctrl" }, "a", true)}
+    elseif char == "Ò" then
+      -- bind alt + shift + l as end of line
+      return true, {hs.eventtap.event.newKeyEvent({ "ctrl" }, "e", true)}
     end
   end
 end
