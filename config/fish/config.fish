2bec2ed69b14c136c8ab23d05ae1284073720466
ahonn <ahonn95@outlook.com>
2019-05-04 23:30:02 +0800

    feat: add flutter proxy

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,10 @@ set -gx TERM 'screen-256color'
 set -gx EDITOR 'nvim'
 set -gx GIT_EDITOR 'nvim'
 
+# flutter
+set -gx PUB_HOSTED_URL https://pub.flutter-io.cn
+set -gx FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
+
 # vi mode
 set -U fish_key_bindings fish_vi_key_bindings
 set -U fish_cursor_insert line
