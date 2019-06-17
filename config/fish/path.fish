0b32be38658e7a5121dd82a6f46d12eeaf8ea5a7
ahonn <ahonn95@outlook.com>
2019-06-17 11:20:38 +0800

    style(fish): format code

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -3,21 +3,21 @@ set -gx ELECTRON_MIRROR https://npm.taobao.org/mirrors/electron/
 
 # =========== PATH =========== #
 if test -d $HOME/bin
-    set -gx fish_user_paths $HOME/bin $fish_user_paths
+  set -gx fish_user_paths $HOME/bin $fish_user_paths
 end
 
 if test -d $DOTFILES/bin
-    set -gx fish_user_paths $DOTFILES/bin $fish_user_paths
+  set -gx fish_user_paths $DOTFILES/bin $fish_user_paths
 end
 
 set -gx GOPATH $HOME/go
 if test -d $GOPATH/bin
-    set -gx fish_user_paths $GOPATH/bin $fish_user_paths
+  set -gx fish_user_paths $GOPATH/bin $fish_user_paths
 end
 
 # flutter
 if test -d $HOME/flutter/bin
-    set -gx fish_user_paths $HOME/flutter/bin $fish_user_paths
+  set -gx fish_user_paths $HOME/flutter/bin $fish_user_paths
 end
 
 # sqlite
@@ -28,15 +28,15 @@ set -gx PKG_CONFIG_PATH "/usr/local/opt/sqlite/lib/pkgconfig"
 
 # rust
 if test -d $HOME/.cargo/bin
-    set -gx fish_user_paths $HOME/.cargo/bin $fish_user_paths
+  set -gx fish_user_paths $HOME/.cargo/bin $fish_user_paths
 end
 
 # ruby rvm
 if test -d $HOME/.rvm
-    set -gx fish_user_paths $HOME/.rvm/bin $fish_user_paths
-    if test -d $HOME/.rvm/gems/ruby-head/bin
-        set -gx fish_user_paths $HOME/.rvm/gems/ruby-head/bin $fish_user_paths
-    end
+  set -gx fish_user_paths $HOME/.rvm/bin $fish_user_paths
+  if test -d $HOME/.rvm/gems/ruby-head/bin
+    set -gx fish_user_paths $HOME/.rvm/gems/ruby-head/bin $fish_user_paths
+  end
 end
 
 # deno
