25b14e8ea7f861960f6443f1cfee076f937c4061
ahonn <ahonn95@outlook.com>
2020-02-04 21:07:45 +0800

    feat: update gitconfig

--- config/fish/conf.d/omf.fish
+++ config/fish/conf.d/omf.fish
@@ -3,8 +3,5 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG '/Users/ahonn/.dotfiles/config/fish/omf'
-
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
