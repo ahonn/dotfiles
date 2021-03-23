b23b8408ad20232323b7c580bdc1873cb243c1b4
ahonn <ahonn95@outlook.com>
2021-03-23 19:28:58 +0800

    feat: adjust setup.sh

--- setup.sh
+++ setup.sh
@@ -7,17 +7,15 @@ echo "Installing dotfiles..."
 echo "Initializing submodule(s)..."
 git submodule update --init --recursive
 
-source $DOTFILES/scripts/link.sh
-source $DOTFILES/scripts/brew.sh
-
 # fish
 chsh -s /usr/local/bin/fish
 curl -L https://get.oh-my.fish | fish
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 
-source $DOTFILES/scripts/gem.sh
-source $DOTFILES/scripts/npm.sh
-source $DOTFILES/scripts/fonts.sh
+source $DOTFILES/scripts/link.sh
 
-source $DOTFILES/scripts/rime.sh
-source $DOTFILES/scripts/hammerspoon.sh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  source $DOTFILES/scripts/brew.sh
+  source $DOTFILES/scripts/rime.sh
+  source $DOTFILES/scripts/hammerspoon.sh
+fi
