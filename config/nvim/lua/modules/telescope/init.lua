d9d41fb3e48af2af016d64438e744a0d1f6465de
ahonn <ahonn95@outlook.com>
2022-09-27 14:18:53 +0800

    feat: update telescope config

--- config/nvim/lua/modules/telescope/init.lua
+++ config/nvim/lua/modules/telescope/init.lua
@@ -3,29 +3,31 @@ local actions = require("telescope.actions")
 local M = {}
 
 function M.setup()
-	require("telescope").load_extension("fzf")
-	require("telescope").setup({
-		defaults = {
-			mappings = {
-				i = {
-					["<C-j>"] = actions.move_selection_next,
-					["<C-k>"] = actions.move_selection_previous,
-					["<Esc>"] = actions.close,
-				},
-			},
-		},
-	})
+  require("telescope").load_extension("fzf")
+  require("telescope").setup({
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-L>"] = actions.cycle_history_next,
+          ["<C-H>"] = actions.cycle_history_prev,
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<Esc>"] = actions.close,
+        },
+      },
+    },
+  })
 
-	local function telescope(func)
-		return "<CMD>lua require('telescope.builtin')." .. func .. "<CR>"
-	end
+  local function telescope(func)
+    return "<CMD>lua require('telescope.builtin')." .. func .. "<CR>"
+  end
 
-	local opts = { noremap = true }
-	vim.api.nvim_set_keymap("n", "<C-p>", telescope("find_files()"), opts)
-	vim.api.nvim_set_keymap("n", "<C-f>", telescope("live_grep()"), opts)
-	vim.api.nvim_set_keymap("n", "<Leader><Space>", telescope("buffers()"), opts)
-	vim.api.nvim_set_keymap("n", "<Leader>d", telescope("diagnostics()"), opts)
-	vim.api.nvim_set_keymap("n", "<Leader>s", telescope("lsp_document_symbols()"), opts)
+  local opts = { noremap = true }
+  vim.api.nvim_set_keymap("n", "<C-p>", telescope("find_files()"), opts)
+  vim.api.nvim_set_keymap("n", "<C-f>", telescope("live_grep()"), opts)
+  vim.api.nvim_set_keymap("n", "<Leader><Space>", telescope("buffers()"), opts)
+  vim.api.nvim_set_keymap("n", "<Leader>d", telescope("diagnostics()"), opts)
+  vim.api.nvim_set_keymap("n", "<Leader>s", telescope("lsp_document_symbols()"), opts)
 end
 
 return M
