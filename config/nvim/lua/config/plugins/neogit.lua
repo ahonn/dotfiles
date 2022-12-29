b4afb5710436e0b74e6de092c92e4747031612b7
ahonn <ahonn95@outlook.com>
2022-12-29 16:40:15 +0800

    feat: add diffview.nvim

--- config/nvim/lua/config/plugins/neogit.lua
+++ config/nvim/lua/config/plugins/neogit.lua
@@ -1,8 +1,14 @@
 return {
   "TimUntersberger/neogit",
   cmd = "Neogit",
+  dependencies = {
+    "sindrets/diffview.nvim",
+  },
   config = {
     kind = "split",
+    integrations = {
+      diffview = true,
+    },
   },
   keys = {
     { "<Leader>gg", "<CMD>Neogit<CR>", desc = "Neogit" },
