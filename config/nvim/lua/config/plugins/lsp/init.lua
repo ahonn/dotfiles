ef13d78b979cd0cedcb81452e047348b46544c4f
ahonn <ahonn95@outlook.com>
2023-07-20 11:31:43 +0800

    feat: using eslint_d to replace eslint

--- config/nvim/lua/config/plugins/lsp/init.lua
+++ config/nvim/lua/config/plugins/lsp/init.lua
@@ -123,15 +123,13 @@ function M.config()
   local null_ls = require("null-ls")
   null_ls.setup({
     sources = {
-      -- null_ls.builtins.code_actions.eslint,
+      null_ls.builtins.code_actions.eslint_d,
       null_ls.builtins.code_actions.refactoring,
-      null_ls.builtins.completion.spell,
-      null_ls.builtins.completion.vsnip,
-      null_ls.builtins.diagnostics.eslint.with({
-        prefer_local = "node_modules/.bin",
-      }),
+      -- null_ls.builtins.completion.spell,
+      -- null_ls.builtins.completion.vsnip,
+      null_ls.builtins.diagnostics.eslint_d,
       null_ls.builtins.formatting.prettier,
-      -- null_ls.builtins.formatting.eslint,
+      null_ls.builtins.formatting.eslint_d,
     },
     on_attach = on_attach,
   })
