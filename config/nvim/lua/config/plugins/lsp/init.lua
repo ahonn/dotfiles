e500ef278a6bd83d5f02aae2b7ee6da7af788819
ahonn <ahonn95@outlook.com>
2024-02-01 09:13:56 +1100

    feat(nvim): add lsp-timeout.nvim

--- config/nvim/lua/config/plugins/lsp/init.lua
+++ config/nvim/lua/config/plugins/lsp/init.lua
@@ -99,12 +99,12 @@ local M = {
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = {
-          null_ls.builtins.code_actions.eslint_d,
+          null_ls.builtins.code_actions.eslint,
           null_ls.builtins.code_actions.refactoring,
-          null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.diagnostics.eslint,
           null_ls.builtins.diagnostics.stylelint,
           null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.eslint_d,
+          null_ls.builtins.formatting.eslint,
           null_ls.builtins.formatting.stylelint,
         },
       })
@@ -126,6 +126,10 @@ local M = {
     requires = {
       "MunifTanjim/nui.nvim",
     }
+  },
+  {
+    "hinell/lsp-timeout.nvim",
+    dependencies = { "neovim/nvim-lspconfig" }
   }
 }
 
