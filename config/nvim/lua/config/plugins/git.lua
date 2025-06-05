fdcfb78b8b6d132a58541a3b8a590b89346c46df
ahonn <yuexunjiang@gmail.com>
2025-06-05 19:52:01 +1000

    feat: customize git-blame highlight to match cursor line

--- config/nvim/lua/config/plugins/git.lua
+++ config/nvim/lua/config/plugins/git.lua
@@ -18,12 +18,20 @@ local M = {
   {
     "f-person/git-blame.nvim",
     event = "VeryLazy",
-    opts = {
-      enabled = true,
-      message_template = "<author> • <date> • <summary>",
-      date_format = "%r",
-      virtual_text_column = 1,
-    },
+    opts = function()
+      local hl_cursor_line = vim.api.nvim_get_hl(0, { name = "CursorLine" })
+      local hl_comment = vim.api.nvim_get_hl(0, { name = "Comment" })
+      local hl_combined = vim.tbl_extend("force", hl_comment, { bg = hl_cursor_line.bg })
+      vim.api.nvim_set_hl(0, "CursorLineBlame", hl_combined)
+
+      return {
+        enabled = true,
+        message_template = "<author> • <date> • <summary>",
+        date_format = "%r",
+        virtual_text_column = 1,
+        highlight_group = "CursorLineBlame",
+      }
+    end
   }
 }
 
