4eb3aa782938af74890783414c62488c38dd9a5c
ahonn <yuexunjiang@gmail.com>
2025-07-28 23:03:30 +1000

    feat: configure Git commit signing

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -1,36 +1,45 @@
-{ lib, pkgs, config, ... }:
-with lib;
-let
-  cfg = config.services.git;
-in {
-  options.services.git = {
-    enable = mkEnableOption "enable";
-  };
+ { lib, pkgs, config, ... }:
+  with lib;
+  let
+    cfg = config.services.git;
+  in {
+    options.services.git = {
+      enable = mkEnableOption "enable";
+    };
 
-  config = mkIf cfg.enable {
-    programs.git = {
-      enable = true;
-      extraConfig = {
-        user = {
-          name = "ahonn";
-          email = "yuexunjiang@gmail.com";
+    config = mkIf cfg.enable {
+      programs.git = {
+        enable = true;
+        extraConfig = {
+          user = {
+            name = "ahonn";
+            email = "yuexunjiang@gmail.com";
+            signingkey = "43EAF127BC5EEB2FA0C4134FBD5538FCA987E538";
+          };
+          core = {
+            ignoreCase = false;
+            excludesfile = "~/.gitignore_global";
+          };
+          push = {
+            default = "current";
+          };
+          alias = {
+            cai = "!git commit -m \"$(claude -p 'Look at the staged git changes and create a summarizing git commit title. Only respond with the title and no affirmation.')\"";
+          };
+          commit = {
+            gpgsign = true;
+          };
+          gpg = {
+            program = "${pkgs.gnupg}/bin/gpg";
+            format = "openpgp";
+            openpgp.program = "${pkgs.gnupg}/bin/gpg";
+          };
         };
-        core = {
-          ignoreCase = false;
-          excludesfile = "~/.gitignore_global";
-        };
-        push = {
-          default = "current";
-        };
-        alias = {
-          cai = "!git commit -m \"$(claude -p 'Look at the staged git changes and create a summarizing git commit title. Only respond with the title and no affirmation.')\"";
-        };
+        difftastic.enable = true;
       };
-      difftastic.enable = true;
-    };
 
-    home.file.".gitignore_global" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../../../symlink/gitignore_global.symlink;
+      home.file.".gitignore_global" = {
+        source = config.lib.file.mkOutOfStoreSymlink ../../../symlink/gitignore_global.symlink;
+      };
     };
-  };
-}
+  }
