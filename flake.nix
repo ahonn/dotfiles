f486b50d7119798993180a7a1115342e4acdcf30
ahonn <yuexunjiang@gmail.com>
2025-10-01 20:59:09 +1000

    fix: make nix-darwin follow main nixpkgs for consistent package versions
    - Configure nix-darwin to use the same nixpkgs as root
    - This enables neovim upgrade from 0.11.1 to 0.11.4
    - Ensures all system packages use consistent nixpkgs version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
     home-manager = {
